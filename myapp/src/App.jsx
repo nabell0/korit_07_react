e4e12716c9bb912489c126d1eaecf91acac71b11
nabell0 <pmy738170@gmail.com>
2025-10-14 16:35:59 +0900

    20251014 react

---
+++ myapp/src/App.jsx
@@ -0,0 +1,14 @@
+// import MyComponent2 from "./MyComponent2"
+import './App.css'
+import Counter from './Counter'
+// import MyComponent from "./MyComponent"
+function App() {
+
+  return (
+    <>
+    <Counter/>
+    </>
+  )
+}
+
+export default App
